6b4a35383d82799cfcbf535c32e0af72a60d2af4
Roberto Buzeta <robertopaipo@hotmail.com>
2026-08-30 10:56:44 -0400

    Alpha-connectingBackend

--- Alexandria-Front-end/src/hooks/useContracts.js
+++ Alexandria-Front-end/src/hooks/useContracts.js
@@ -1,23 +1,3 @@
-<<<<<<< Updated upstream
-import { useMemo } from 'react'
-import { Contract } from 'ethers'
-import { useWallet } from '../context/WalletContext'
-import { ADDRESSES, ABIS } from '../config/contracts'
-
-export function useContracts() {
-  const { signer } = useWallet()
-
-  return useMemo(() => {
-    if (!signer) return null
-    return {
-      token:   new Contract(ADDRESSES.token,   ABIS.token,   signer),
-      library: new Contract(ADDRESSES.library, ABIS.library, signer),
-      stake:   new Contract(ADDRESSES.stake,   ABIS.stake,   signer),
-      rent:    new Contract(ADDRESSES.rent,    ABIS.rent,    signer),
-      payment: new Contract(ADDRESSES.payment, ABIS.payment, signer),
-    }
-  }, [signer])
-=======
 import { useMemo } from 'react';
 import { Contract } from 'ethers';
 import { useWallet } from '../context/WalletContext';
@@ -43,5 +23,4 @@ export function useContracts() {
   }, [provider, signer]);
 
   return contracts;
->>>>>>> Stashed changes
 }
